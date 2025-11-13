9eac1a8318c32c7198a9176e97d048874d6cdaa5
“Sean <sean.she@berkeley.edu>
2025-11-12 18:45:42 -0800

    mobile view tweaks

--- src/components/resume-card.tsx
+++ src/components/resume-card.tsx
@@ -69,62 +69,62 @@ export const ResumeCard = ({
         {description ? (
           <div className="flex-grow ml-4 items-center flex-col group relative ">
             <CardHeader>
-            <div className="flex items-center justify-between gap-x-2 text-base">
-              <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
-                {title}
-                {badges && (
-                  <span className="inline-flex gap-x-1">
-                    {badges.map((badge, index) => (
-                      <Badge
-                        variant="secondary"
-                        className="align-middle text-xs"
-                        key={index}
-                      >
-                        {badge}
-                      </Badge>
-                    ))}
-                  </span>
-                )}
-                <ChevronRightIcon
-                  className={cn(
-                    "size-4 translate-x-0 transform opacity-0 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:opacity-100",
-                    isExpanded ? "rotate-90" : "rotate-0"
+              <div className="flex items-center justify-between gap-x-2 text-base">
+                <h3 className="inline-flex items-center justify-center font-semibold leading-tight text-xs sm:text-sm">
+                  {title}
+                  {badges && (
+                    <span className="inline-flex gap-x-1">
+                      {badges.map((badge, index) => (
+                        <Badge
+                          variant="secondary"
+                          className="align-middle text-xs"
+                          key={index}
+                        >
+                          {badge}
+                        </Badge>
+                      ))}
+                    </span>
                   )}
-                />
-              </h3>
-              <div className="text-xs sm:text-sm tabular-nums text-muted-foreground text-right">
-                {period}
+                  <ChevronRightIcon
+                    className={cn(
+                      "size-4 translate-x-0 transform opacity-0 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:opacity-100",
+                      isExpanded ? "rotate-90" : "rotate-0"
+                    )}
+                  />
+                </h3>
+                <div className="text-xs sm:text-sm tabular-nums text-muted-foreground text-right leading-tight">
+                  {period}
+                </div>
               </div>
-            </div>
-            {subtitle && <div className="font-sans text-xs sm:text-sm">{subtitle}</div>}
-          </CardHeader>
-          {description && (
-            <motion.div
-              initial={{ opacity: 0, height: 0 }}
-              animate={{
-                opacity: isExpanded ? 1 : 0,
+              {subtitle && <div className="font-sans text-xs sm:text-sm mt-[2px]">{subtitle}</div>}
+            </CardHeader>
+            {description && (
+              <motion.div
+                initial={{ opacity: 0, height: 0 }}
+                animate={{
+                  opacity: isExpanded ? 1 : 0,
 
-                height: isExpanded ? "auto" : 0,
-              }}
-              transition={{
-                duration: 0.7,
-                ease: [0.16, 1, 0.3, 1],
-              }}
-              className="mt-2 text-xs sm:text-sm"
-            >
-              <Markdown 
-                className="prose max-w-full text-pretty font-sans text-xs sm:text-sm text-muted-foreground dark:prose-invert"
-                components={{
-                  a: ({ ...props }) => (
-                    <a {...props} target="_blank" rel="noopener noreferrer" />
-                  ),
+                  height: isExpanded ? "auto" : 0,
                 }}
+                transition={{
+                  duration: 0.7,
+                  ease: [0.16, 1, 0.3, 1],
+                }}
+                className="mt-2 text-xs sm:text-sm"
               >
-                {description}
-              </Markdown>
-            </motion.div>
-          )}
-        </div>
+                <Markdown 
+                  className="prose max-w-full text-pretty font-sans text-xs sm:text-sm text-muted-foreground dark:prose-invert"
+                  components={{
+                    a: ({ ...props }) => (
+                      <a {...props} target="_blank" rel="noopener noreferrer" />
+                    ),
+                  }}
+                >
+                  {description}
+                </Markdown>
+              </motion.div>
+            )}
+          </div>
         ) : (
           <Link 
             href={href || "#"} 
@@ -134,7 +134,7 @@ export const ResumeCard = ({
           >
             <CardHeader>
               <div className="flex items-center justify-between gap-x-2 text-base">
-                <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
+                <h3 className="inline-flex items-center justify-center font-semibold leading-tight text-xs sm:text-sm">
                   {title}
                   {badges && (
                     <span className="inline-flex gap-x-1">
@@ -154,7 +154,7 @@ export const ResumeCard = ({
                   {period}
                 </div>
               </div>
-              {subtitle && <div className="font-sans text-xs sm:text-sm">{subtitle}</div>}
+              {subtitle && <div className="font-sans text-xs sm:text-sm mt-[2px]">{subtitle}</div>}
             </CardHeader>
           </Link>
         )}
